272e08bb1d8b2136b98650d89ba39dacaafeef41
Gavin Calkins <gavin.calkins@gmail.com>
2020-12-14 12:29:00 -0800

    updated file paths for variables in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Manager = require("./Assets/Library/Manager");
+const Engineer = require("./Assets/Library/Engineer");
+const Intern = require("./Assets/Library/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -8,7 +8,7 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+const render = require("./Assets/Library/htmlRenderer");
 
 
 // Write code to use inquirer to gather information about the development team members,
